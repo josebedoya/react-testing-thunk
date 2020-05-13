f6f08a2c486e894817e255103d211951beabbf82
Jose Bedoya <jossephb@gmail.com>
2020-05-13 12:15:09 -0500

    initial commit

---
+++ src/reducers/usuarios.test.js
@@ -0,0 +1,58 @@
+import { fetchUsuarios } from './usuarios';
+
+describe('Duck usuarios', () => {
+  describe('fetchUsuarios', () => {
+    it('success', async () => {
+      const dispatch = jest.fn();
+      const getState = jest.fn();
+      const services = {
+        axios: {
+          get: jest.fn().mockResolvedValue({
+            data: 1,
+          }),
+        },
+      };
+      await fetchUsuarios()(dispatch, getState, services);
+      expect(dispatch.mock.calls).toEqual([
+        [
+          {
+            type: 'FETCH_USUARIOS_START',
+            error: false,
+          },
+        ],
+        [
+          {
+            type: 'FETCH_USUARIOS_SUCCESS',
+            payload: 1,
+          },
+        ],
+      ]);
+    });
+
+    it('failure', async () => {
+      const dispatch = jest.fn();
+      const getState = jest.fn();
+      const services = {
+        axios: {
+          get: jest.fn().mockRejectedValue(1),
+        },
+      };
+      await fetchUsuarios()(dispatch, getState, services);
+      expect(dispatch.mock.calls).toEqual([
+        [
+          {
+            type: 'FETCH_USUARIOS_START',
+            error: false,
+          },
+        ],
+        [
+          {
+            type: 'FETCH_USUARIOS_ERROR',
+            payload: 1,
+            error: true
+          },
+        ],
+      ]);
+    });
+  });
+});
